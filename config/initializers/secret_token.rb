8920e4400209e0be1dc796141ac4d6a3dc5fcdce
kapil <kapil@venturit.com>
2013-02-02 20:29:34 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billshare::Application.config.secret_token = '511b1ff04f88de988726200008bdb1af722781438298b85bb2842c19b12b613c45ed528f97ef591f198cef6156937459dbfe0d9a8f27b56890f6dfd2da4374f9'
